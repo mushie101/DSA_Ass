79873e00b978c12aada6fb2955d2594838a6dd6f
Mustafa Siddiqui <mustafasiddiqui@pop-os.localdomain>
2020-10-06 07:36:39 +0530

    minor nav tweak

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ import sys
 from dB import cursor
 import subprocess as sp
 
+def deletion():
+    print('======================= Deletion =======================',end="\n\n")
+    
+
+
+def deletion_menu():
+    cursor.execute("USE _ctf_c")
+    sp.call('clear',shell=True)
+    print('======================= Deletion Menu =======================')
+    print('1)--> Demolition of a Venue')
+    print('-------------------------------------------------------------')
+    inp = int(input("Enter Option:- "))
+    deletion(inp)
+
+
 def generate_report(inp):
     cursor.execute("USE _ctf_c")
     if inp == 6:
@@ -15,58 +30,39 @@ def generate_report(inp):
         print("Number of wins per team [wins, teamID]")
         sql = "SELECT COUNT(M.WonBy) \"COUNT\", T.Team_ID FROM `match` as M, `team` as T WHERE WonBy IS NOT NULL AND T.Team_ID = M.WonBy GROUP BY T.Team_ID"
         print("")
-        cursor.execute(sql)
-        result=cursor.fetchall()
-        print(result)
-        time.sleep(3)
-        main_menu()
     
     elif inp==2:
         print('======================= Report =======================',end="\n\n")
         print('Best Attacker of the competition [Player ID, Username]')
         sql = "SELECT Player_ID, Username from player WHERE Player_ID = (SELECT Player_ID from attacker WHERE points = (SELECT MAX(Points) FROM attacker))"
         print("")
-        cursor.execute(sql)
-        result=cursor.fetchall()
-        print(result)
-        time.sleep(3)
-        main_menu()
     
     elif inp==3:
         print('======================= Report =======================',end="\n\n") 
         print('Best Defender of the competition [Player ID, Username]')
         sql = "SELECT Player_ID, Username FROM player WHERE Player_ID = (SELECT Player_ID FROM defender WHERE Traps_Triggered = (SELECT MAX(Traps_Triggered) FROM defender))"  
-        print("")
-        cursor.execute(sql)
-        result=cursor.fetchall()
-        print(result)
-        time.sleep(3)
-        main_menu()           
+        print("")         
 
     elif inp==4:
         print('======================= Report =======================',end="\n\n") 
         print('Highest Individual Scorer of Competition [Player ID, Username]')
         sql = "SELECT Player_ID, Username FROM player WHERE Player_ID=(SELECT Player_ID FROM attacker WHERE Highscore = (SELECT MAX(Highscore) FROM attacker))"  
-        print("")
-        cursor.execute(sql)
-        result=cursor.fetchall()
-        print(result)  
-        time.sleep(3)
-        main_menu()      
+        print("")     
 
     elif inp==5:
         print('======================= Report =======================',end="\n\n") 
         print('Number of all rounders in the competition (both attackers and defenders)')
         sql = "SELECT COUNT(*) FROM (SELECT PLayer_ID, COUNT(Player_ID) FROM player_type GROUP BY Player_ID HAVING COUNT(Player_ID)=2) A"  
         print("")
-        cursor.execute(sql)
-        result=cursor.fetchall()
-        print(result)
-        time.sleep(3)
-        main_menu()
 
     else:
-        report_menu()   
+        report_menu()  
+    
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    print(result)
+    inp = input("Enter any key to go back to the main menu> ")
+    main_menu() 
 
 
 
@@ -90,11 +86,11 @@ def logout():
 
 def main_menu_redirection(inp):
     if inp == 1:
-        insertion()
+        insertion_menu()
     elif inp == 2:
-        updation()
+        updation_menu()
     elif inp == 3:
-        deletion()
+        deletion_menu()
     elif inp == 4:
         report_menu()
     elif inp == 5:
